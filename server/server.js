443d6fc3312ca943cd417ed8c53c8451ad754d0b
Aryaan007-sudo <blankgaming75@gmail.com>
2025-08-05 10:56:31 +0530

    backend url

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? [process.env.FRONTEND_URL] 
-    : ['http://localhost:5000', 'http://localhost:5173'],
+    : ['https://medi-care-pro-backend.vercel.app/', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
